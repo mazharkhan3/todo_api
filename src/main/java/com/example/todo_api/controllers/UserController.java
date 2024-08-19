f770b86b7b121b6daaec7190f70d4a7de9487fdc
Mazhar Khan <mazharwhite@hotmail.com>
2024-08-19 19:04:47 +0500

    code refactoring

--- src/main/java/com/example/todo_api/controllers/UserController.java
+++ src/main/java/com/example/todo_api/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.todo_api.controllers;
 import com.example.todo_api.data.User;
 import com.example.todo_api.models.users.CreateUserDTO;
 import com.example.todo_api.repository.UserRepository;
+import com.example.todo_api.util.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -22,14 +23,14 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> Register(@RequestBody CreateUserDTO userDTO){
+    public ResponseEntity<ApiResponse> Register(@RequestBody CreateUserDTO userDTO){
         var user = new User();
         user.setName(userDTO.getName());
         user.setEmail(userDTO.getEmail());
         user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         user.setRole("USER");
         userRepository.save(user);
-        return ResponseEntity.ok("Registered");
+        return ResponseEntity.ok(new ApiResponse("User created", null));
     }
 
 }
--- src/main/java/com/example/todo_api/models/tasks/UpdateTaskDTO.java
+++ src/main/java/com/example/todo_api/models/tasks/UpdateTaskDTO.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UpdateTaskDTO {
     private String title;
     private String description;
